5ebf38d56f26aeb98cbcd555aad00f13a1ddb4ec
Korczak <contaparameulove@gmail.com>
2026-09-13 19:31:57 -0300

    refactor(rpg): cria pacote modular Luta

---
+++ comandos/RPG/Luta/__init__.py
@@ -0,0 +1,10 @@
+"""Pacote modular do sistema de combate.
+
+Responsabilidades:
+- Infos_Luta: regras, dados e consultas de combate.
+- Mensagens_luta: interface visual e textos/embeds.
+- sistemas_luta: motor, estado e resolução dos combates.
+- comandos_luta: comandos públicos !luta, !soco, !defesa etc.
+"""
+
+__all__ = ["Infos_Luta", "Mensagens_luta", "sistemas_luta", "comandos_luta"]
