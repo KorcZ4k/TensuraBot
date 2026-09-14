4678cbeec4c1eb9cfcbacf1fbb803a5a11558ad4
Korczak <contaparameulove@gmail.com>
2026-09-14 15:58:03 -0300

    Liga comandos de combate ao fluxo UI corrigido

--- comandos/RPG/Luta/__init__.py
+++ comandos/RPG/Luta/__init__.py
@@ -7,4 +7,8 @@ Responsabilidades:
 - comandos_luta: comandos públicos !luta, !soco, !defesa etc.
 """
 
+# Carrega a ponte de compatibilidade antes dos comandos públicos.
+# Ela redireciona ataque/defesa para o fluxo de mensagem única.
+from . import ui_fix  # noqa: F401,E402
+
 __all__ = ["Infos_Luta", "Mensagens_luta", "sistemas_luta", "comandos_luta"]
