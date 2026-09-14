ab0f955b2796527315161bf7f099fa5c476d6285
Korczak <contaparameulove@gmail.com>
2026-09-14 20:51:51 -0300

    Corrige caminho de ataque dos monstros na UI

--- comandos/RPG/Luta/ui_fix.py
+++ comandos/RPG/Luta/ui_fix.py
@@ -161,6 +161,27 @@ async def _executar_defesa_ui(self, ctx, acao, embed=None):
         return
 
 
+async def _criar_ataque_monstro_ui_seguro(self, combate):
+    """Usa o mesmo caminho de ataque do motor/balanceamento na UI.
+
+    O fluxo antigo recriava o golpe diretamente e, por isso, pulava
+    _ataque_monstro do balanceamento: bosses perdiam habilidades especiais.
+    """
+    atacante = self._obter_atacante(combate)
+    defensor = self._obter_defensor(combate)
+    if not atacante or atacante.get("tipo") != "monstro" or not defensor:
+        return
+    await self._ataque_monstro(self._ui_context(self._ui_context_original(combate), combate))
+
+
+def _ui_context_original(self, combate):
+    """Recupera um objeto adequado para o _UIContext sem depender de ctx global."""
+    mensagem = combate.get("ui_message")
+    if mensagem is None:
+        return self.bot
+    return mensagem
+
+
 # O balanceamento chama _regras_monstro a partir dos wrappers de dano.
 # O metodo real pertence ao modulo de balanceamento; a ponte evita importar
 # o simbolo com `from ... import`, que quebra quando o balanceamento o mantem
@@ -175,4 +196,5 @@ Luta._ataque_jogador = _ataque_jogador_ui
 Luta._defesa_jogador = _defesa_jogador_ui
 Luta.executar_defesa_jogador = _executar_defesa_ui
 Luta._ui_editar = _ui_editar_seguro
+Luta._criar_ataque_monstro_ui = _criar_ataque_monstro_ui_seguro
 _UIContext.send = _ui_context_send_seguro
