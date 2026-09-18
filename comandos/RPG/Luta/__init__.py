cd26f67afca4e0620985269d863699383088d27b
Korczak <contaparameulove@gmail.com>
2026-09-17 22:06:11 -0300

    fix: usar recompensa central do motor efetivo

--- comandos/RPG/Luta/__init__.py
+++ comandos/RPG/Luta/__init__.py
@@ -4,7 +4,6 @@ O motor efetivo é hardening_final.Luta. Os módulos antigos de monkey-patch
 não são importados aqui para evitar múltiplos donos do mesmo método.
 """
 from . import contrato_monstro
-from . import recompensa_fix
 from . import hardening_final
 from . import compat_ui
 
