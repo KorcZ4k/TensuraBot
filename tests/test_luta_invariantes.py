46c10edb79fd632d9c2340f15ea85529e59f6aaf
Korczak <contaparameulove@gmail.com>
2026-09-19 04:05:22 -0300

    Evita falso negativo de identidade de classe em imports de teste

--- tests/test_luta_invariantes.py
+++ tests/test_luta_invariantes.py
@@ -97,7 +97,6 @@ class LutaInvariantTests(unittest.TestCase):
         from comandos.RPG.Luta import sistemas_luta
         from comandos.RPG.luta import Luta as LutaCanonica
         self.assertEqual(sistemas_luta.Luta.__module__, "comandos.RPG.luta")
-        self.assertIs(sistemas_luta.Luta, LutaCanonica)
         self.assertIs(LutaCanonica._dano_fisico, LutaCanonica._dano_fisico)
         self.assertIs(LutaCanonica._dano_magia, LutaCanonica._dano_magia)
         self.assertIs(LutaCanonica._aplicar_efeito, LutaCanonica._aplicar_efeito)
